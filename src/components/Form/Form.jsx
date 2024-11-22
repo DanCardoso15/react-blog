6e039f63a37eea64bff4beada3c3968a457451f6
DanCardoso15 <dancardoso15@hotmail.com>
2024-11-22 17:44:12 -0300

    Atividade - Aula 22

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,35 +1,60 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Form() {
-    const [nome, setNome] = useState(""); // O que está sendo feito aqui representa um "estado" -> Guarda a informação 
+  // const [nome, setNome] = useState(""); // O que está sendo feito aqui representa um "estado" -> Guarda a informação
 
-    function enviarFormulario() {
-        window.alert("Formulário enviado.");
-    }
+  // function enviarFormulario() {
+  //     window.alert("Formulário enviado.");
+  // }
 
-    function lidarMudancaNome(event) {
-        const valor = event.target.value;
-        setNome(valor);
-    }
+  // function lidarMudancaNome(event) {
+  //     const valor = event.target.value;
+  //     setNome(valor);
+  // }
 
-    return (
-      <form style={{ padding: "20px" }}>
-        <p>{nome}</p>
+  const { handleSubmit, register } = useForm();
 
+  function enviarFormulario(dados) {
+    console.log("Formulário enviado.");
+    console.log(dados);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(enviarFormulario)} style={{ padding: "20px" }}>
+      
+      <div>
         <label htmlFor="nome">Nome</label> <br />
-        <input type="text" id="nome" onChange={lidarMudancaNome} /> <br />
+        <input type="text" id="nome" {...register("nome", {
+            required: true,
+          })} /* onChange={lidarMudancaNome} */
+        />
+      </div>
 
+      <div>
         <label htmlFor="email">Email</label> <br />
-        <input type="Email" id="Email" /> <br />
+        <input type="email" id="email" {...register("email", {
+            required: true,
+            minLength: 10,
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          })}
+        />
+      </div>
 
+      <div>
         <label htmlFor="mensagem">Mensagem</label> <br />
-        <input type="text" id="mensagem" /> <br />
+        <input type="text" id="mensagem" {...register("mensagem", {
+            required: true,
+            maxLength: 180,
+          })}
+        />
+      </div>
 
-        <button type="button" onClick={enviarFormulario}>Enviar</button>
-      </form> 
-    );
+      <button /* onClick={enviarFormulario} */>Enviar</button>
+    </form>
+  );
 }
 
 export default Form;
 
-// Para ver a lista de eneventos basta digitar "on" e o comando "Ctrl + espaço" 
+// Para ver a lista de eneventos basta digitar "on" e o comando "Ctrl + espaço"
